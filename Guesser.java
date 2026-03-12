a0fe009542c3042916847ca92cf8d8698e5c1722
ellaccrites <ellac.crites2006@gmail.com>
2026-03-12 22:02:16 +0000

    finished computer guesser

--- Guesser.java
+++ Guesser.java
@@ -28,6 +28,7 @@ public class Guesser {
 		System.out.print("Enter 0, 1, or 2: ");
 		String userChoice = input.nextLine();
 		
+		System.out.println();	
 		return userChoice;
 	}// end menu
 	
@@ -37,8 +38,8 @@ public class Guesser {
 		
 		System.out.println("Guess a number 1-100... ");
 		
-		int keepGoing = 1;
-		while(keepGoing == 1){
+		boolean keepGoing = true;
+		while(keepGoing){
 			int guess = 0;
 			try {
 			System.out.print("Enter a number: ");
@@ -49,20 +50,56 @@ public class Guesser {
 			} catch (NumberFormatException e){
 				System.out.println("Please input a number like: 0, 1, 2 etc");
 			}// end try
-
+			
 			if(guess < correct){
 				System.out.println("Too low.");
 			} else if(guess > correct){
 				System.out.println("Too high.");
 			} else if(guess == correct){
 				System.out.println("You got it! Well done.");
-				keepGoing = 0;
+				keepGoing = false;
 			}// end else if
 		}// end while
+		System.out.println();
 	}// end humanGuesser
 	
 	public void computerGuesser(){
-		System.out.println("Computer guesser is blank.");
+		System.out.println("Think of a number from 1-100...");
+		System.out.println("After each guess, enter 'h' for too high, 'l' for too low, or 'c' for correct!");
+		System.out.println();
+
+		int lower = 0;
+		int upper = 100;
+		int guess = 50;
+		System.out.println("Computer guesses: " + guess);
+
+		boolean keepGoing = true;
+		while(keepGoing){
+			System.out.print("Too high, too low, or correct? ");
+			Scanner input = new Scanner(System.in);
+			String status = input.nextLine();
+			System.out.println();
+			
+			status = status.toLowerCase();
+			if(status.equals("h")){
+				upper = guess;
+				double dbGuess = guess - ((guess - lower) * 0.5);
+				guess = (int) dbGuess;
+				System.out.println("Computer guesses: " + guess);
+			} else if(status.equals("l")){
+				lower = guess;
+				double dbGuess = guess + ((upper - guess) * 0.5);
+                                guess = (int) dbGuess;
+				System.out.println("Computer guesses: " + guess);
+			} else if(status.equals("c")){
+				System.out.println("Computer guessed the number!");
+				keepGoing = false;
+			} else {
+				System.out.println("Please enter h, l, or c");
+			}// end else if
+
+		}// end while
+	System.out.println();
 	}// end computerGuesser
 }// end class def
 
